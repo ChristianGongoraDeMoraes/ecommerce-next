0690a26c19869a663b60a6dbfffc8d5de715998f
chris <christianpimpo304@hotmail.com>
2025-11-03 15:28:46 -0300

    fix: remove position at cart page

--- app/(private)/cart/ItemCart.tsx
+++ app/(private)/cart/ItemCart.tsx
@@ -23,13 +23,13 @@ const ItemCard = (props: ItemCart) => {
           className='w-[300px] h-[200px] rounded-lg'
         />
       </CardContent>
-      <div className='w-[100%]'>
-        <CardHeader className='pt-3'>
-          <CardTitle>{props.name}</CardTitle>
+      <div className='w-[100%] flex flex-col justify-between'>
+        <CardHeader className='pt-3 flex flex-col justify-between'>
+          <CardTitle >{props.name}</CardTitle>
           <CardDescription >{props.description}</CardDescription>
-          <CardDescription className='text-black font-medium'>{props.price}</CardDescription>
+          <CardDescription className='text-black font-medium'>R$: {props.price}</CardDescription>
         </CardHeader>
-        <CardFooter className='gap-3 py-3 flex flex-wrap'>
+        <CardFooter className='gap-3 py-3 flex flex-wrap justify-end'>
           <Button 
           onClick={()=>{props.handleRemoveToCart({
             id: props.id,
@@ -38,7 +38,7 @@ const ItemCard = (props: ItemCart) => {
             amount_on_storage: props.amount_on_storage,
             description: props.description,
           })}}
-          className='bg-transparent bg-gradient-to-br from-red-500 to-white-500 text-white focus-visible:ring-pink-600/20'>
+          className='mr-[10px] bg-transparent bg-gradient-to-br from-red-500 to-white-500 text-white focus-visible:ring-pink-600/20'>
             Remove
           </Button>
         </CardFooter>
